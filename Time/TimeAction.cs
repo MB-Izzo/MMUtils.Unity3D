356e400c0c82a3f85ccf25877fbece7dc0f120eb
Laurent Victorino <lvictorino@monkeymoon.net>
2017-12-20 17:11:32 +0100

    Time Manager & Time Action.

---
+++ Time/TimeAction.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace MM.Utils
+{
+    public enum eTimeActionStatus
+    {
+        running,
+        paused,
+        finished
+    }
+
+    /// <summary>
+    /// Define the state of an action to be done in time.
+    /// Given a start time and a duration this object is updated (if running) and call
+    /// Actions when it's updated or finished.
+    /// </summary>
+    public class TimeAction
+    {
+        private Action<float> _update_callback;
+        private Action _end_callback;
+        private float _time_elapsed;
+
+        public float start_time { get; private set; }
+        public float duration { get; private set; }
+        public float ratio { get { return _time_elapsed / duration; } }
+        public eTimeActionStatus status { get; private set; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="duration">Duration of the action.</param>
+        /// <param name="update_callback">Action to be called when the object is updated.</param>
+        /// <param name="end_callback">Action to be called when the action is finished.</param>
+        public TimeAction(float duration, Action<float> update_callback = null, Action end_callback = null)
+        {
+            this.duration = duration;
+            start_time = TimeManager.instance.game_time;
+            status = eTimeActionStatus.running;
+            _time_elapsed = 0.0f;
+            _update_callback = update_callback;
+            _end_callback = end_callback;
+        }
+
+        /// <summary>
+        /// Called by Time Manager to update the object if needed.
+        /// Only "running" TimeAction will be updated.
+        /// </summary>
+        /// <param name="delta">Delta time to update the TimeAction with.</param>
+        /// <returns>Current status of the TimeAction object.</returns>
+        public eTimeActionStatus Update(float delta)
+        {
+            // if the TimeAction is running...
+            if (status == eTimeActionStatus.running)
+            {
+                _time_elapsed += delta;
+                // clamp _elapsed_time to duration if needed.
+                if (_time_elapsed > duration)
+                    _time_elapsed = duration;
+
+                // if the action is not finished...
+                if (ratio != 1.0f)
+                {
+                    // call the update_callback action if needed.
+                    if (_update_callback != null)
+                        _update_callback(ratio);
+                }
+                else
+                {
+                    Stop();
+                }
+            }
+            return status;
+        }
+
+        /// <summary>
+        /// Pause the TimeAction.
+        /// </summary>
+        public void Pause()
+        {
+            if (status != eTimeActionStatus.finished)
+                status = eTimeActionStatus.paused;
+        }
+
+        /// <summary>
+        /// Resume the TimeAction.
+        /// </summary>
+        public void Resume()
+        {
+            if (status != eTimeActionStatus.finished)
+                status = eTimeActionStatus.running;
+        }
+
+        /// <summary>
+        /// Stop the TimeAction.
+        /// </summary>
+        public void Stop()
+        {
+            if (_end_callback != null)
+                _end_callback();
+            status = eTimeActionStatus.finished;
+        }
+    }
+}
---
+++ Time/TimeManager.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+/// TODO
+/// + speed factor for a given group
+/// + remove a group if empty
+
+namespace MM.Utils
+{
+    public enum eTimeGroup
+    {
+        common,
+        map,
+        map_ui,
+    }
+
+    /// <summary>
+    /// Main access point to Time.
+    /// Allows one to use several / different in game timers.
+    /// Allows to pause/resume/stop an entire group of TimeActions.
+    /// </summary>
+    public class TimeManager : MonoBehaviour
+    {
+        /// <summary>
+        /// TimeActions grouped by group type.
+        /// </summary>
+        private Dictionary<eTimeGroup, List<TimeAction>> _time_actions;
+        /// <summary>
+        /// Game time: time since the Time Manager is running.
+        /// </summary>
+        public float game_time { get; private set; }
+
+        public static TimeManager instance { get; private set; }
+
+        private void Awake()
+        {
+            instance = this;
+            _time_actions = new Dictionary<eTimeGroup, List<TimeAction>>();
+        }
+
+        /// <summary>
+        /// Add a new TimeAction to a group.
+        /// </summary>
+        /// <param name="new_action">New TimeAction to add.</param>
+        /// <param name="group">Group where the TimeAction will be added.</param>
+        public void AddAction(TimeAction new_action, eTimeGroup group = eTimeGroup.common)
+        {
+            // If group doesn't exist, create it.
+            if (_time_actions.ContainsKey(group) == false)
+                _time_actions.Add(group, new List<TimeAction>());
+
+            if (new_action != null)
+            {
+                _time_actions[group].Add(new_action);
+            }
+            else
+            {
+                Debug.LogError("Can't add null TimeAction to TimeManager.");
+            }
+        }
+
+        /// <summary>
+        /// Pauses an entire group of TimeAction.
+        /// </summary>
+        /// <param name="group">Group to pause.</param>
+        public void PauseActionGroup(eTimeGroup group)
+        {
+            if (_time_actions.ContainsKey(group) == true)
+            {
+                List<TimeAction> actions = _time_actions[group];
+                foreach (TimeAction ta in actions)
+                {
+                    ta.Pause();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Resumes an entire group of TimeAction.
+        /// </summary>
+        /// <param name="group">Group to resume.</param>
+        public void ResumeActionGroup(eTimeGroup group)
+        {
+            if (_time_actions.ContainsKey(group) == true)
+            {
+                List<TimeAction> actions = _time_actions[group];
+                foreach (TimeAction ta in actions)
+                {
+                    ta.Resume();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Stops an entire group of TimeAction.
+        /// Will remove them from the dictionnary.
+        /// </summary>
+        /// <param name="group"></param>
+        public void StopActionGroup(eTimeGroup group)
+        {
+            if (_time_actions.ContainsKey(group) == true)
+            {
+                List<TimeAction> actions = _time_actions[group];
+                foreach (TimeAction ta in actions)
+                {
+                    ta.Stop();
+                }
+            }
+        }
+
+        /// <summary>
+        /// MonoBehaviour.Update()
+        /// </summary>
+        private void Update()
+        {
+            game_time += Time.deltaTime;
+
+            List<TimeAction> finished_actions;
+            // Get TimeAction List for every TimeAction group
+            foreach (KeyValuePair<eTimeGroup, List<TimeAction>> pair in _time_actions)
+            {
+                finished_actions = new List<TimeAction>();
+                foreach (TimeAction ta in pair.Value)
+                {
+                    // Update every TimeAction
+                    if ( ta.Update(Time.deltaTime) == eTimeActionStatus.finished )
+                    {
+                        finished_actions.Add(ta);
+                    }
+                }
+
+                // Remove finished actions
+                foreach( TimeAction ta in finished_actions )
+                {
+                    pair.Value.Remove(ta);
+                }
+            }
+        }
+    }
+}
